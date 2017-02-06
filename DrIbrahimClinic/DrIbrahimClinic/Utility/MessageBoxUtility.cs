d73c3b9115b85ac0a6180a3d44cfe49fb474e55f
ANGELOS-PC\Angelos <angelos.m.reda@gmail.com>
2017-02-06 19:53:22 +0200

    adding MessageBoxUtility

---
+++ DrIbrahimClinic/DrIbrahimClinic/Utility/MessageBoxUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace DrIbrahimClinic.Utility
+{
+    static class MessageBoxUtility
+    {
+        public static void ShowErrorMsg(string msg)
+        {
+            MessageBox.Show(msg, "خطأ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public static void ShowWarningMsg(string msg)
+        {
+            MessageBox.Show(msg, "تحذير", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
