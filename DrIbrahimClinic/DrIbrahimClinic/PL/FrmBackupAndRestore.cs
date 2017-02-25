054aaa7575d3a90000208573055b3029248a41f9
ANGELOS-PC\Angelos <angelos.m.reda@gmail.com>
2017-02-25 23:55:43 +0200

    editing FrmBackupAndRestore form

--- DrIbrahimClinic/DrIbrahimClinic/PL/FrmBackupAndRestore.cs
+++ DrIbrahimClinic/DrIbrahimClinic/PL/FrmBackupAndRestore.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 using DrIbrahimClinic.BLL;
 using static DrIbrahimClinic.Utility.MessageBoxUtility;
 
@@ -20,34 +21,49 @@ namespace DrIbrahimClinic.PL
 
         #region Events
 
-        private void btnBackup_Click(object sender, System.EventArgs e)
+        private void btnBackup_Click(object sender, EventArgs e)
         {
             Cursor = Cursors.WaitCursor;
-            var folderBrowserDialog = new FolderBrowserDialog
+            try
             {
-                Description = @"من فضلك قم بإختيار المكان الذي تريد نسخ قاعدة البيانات فية"
-            };
-            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
-                DatabaseManager.Backup(folderBrowserDialog.SelectedPath);
-            ShowInfoMsg("تم نسخ قاعدة البيانات بنجاح");
+                var folderBrowserDialog = new FolderBrowserDialog
+                {
+                    Description = @"من فضلك قم بإختيار المكان الذي تريد نسخ قاعدة البيانات فية"
+                };
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                    DatabaseManager.Backup(folderBrowserDialog.SelectedPath);
+                ShowInfoMsg("تم نسخ قاعدة البيانات بنجاح");
+                Close();
+            }
+            catch
+            {
+                ShowErrorMsg("حدث خطأ ما، من فضلك حاول مرة أخري ... قم بتغيير المكان");
+            }
             Cursor = Cursors.Default;
-            Close();
         }
 
-        private void btnRestore_Click(object sender, System.EventArgs e)
+        private void btnRestore_Click(object sender, EventArgs e)
         {
+            //TODO: repository method not implemented completely
             Cursor = Cursors.WaitCursor;
-            var openFileDialog = new OpenFileDialog
+            try
             {
-                Title = @"من فضلك قم بإختيار ملف قاعدة البيانات الذي تريد إسترجاعة",
-                Filter = @"bak files (.bak)|*.bak",
-                Multiselect = false
-            };
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-                DatabaseManager.Restore(openFileDialog.FileName);
-            ShowInfoMsg("تم إستعادة قاعدة البيانات بنجاح");
+                var openFileDialog = new OpenFileDialog
+                {
+                    Title = @"من فضلك قم بإختيار ملف قاعدة البيانات الذي تريد إسترجاعة",
+                    Filter = @"bak files (.bak)|*.bak",
+                    Multiselect = false
+                };
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    DatabaseManager.Restore(openFileDialog.FileName);
+                ShowInfoMsg("تم إستعادة قاعدة البيانات بنجاح");
+                Close();
+            }
+            catch
+            {
+                ShowErrorMsg("حدث خطأ ما، من فضلك حاول مرة أخري ... قم بتغيير المكان");
+            }
             Cursor = Cursors.Default;
-            Close();
         }
 
         #endregion
